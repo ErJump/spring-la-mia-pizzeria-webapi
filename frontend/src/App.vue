f02a826ce487b1cee60d794ff5460cc4d00b2765
ErJump <fedelijunior@gmail.com>
2022-12-19 12:48:48 +0100

    feat: :sparkles: Adds getPizzas method

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,13 +1,39 @@
 <template>
   <div id="app">
     <h1>Pizze</h1>
+    <ul>
+      <li v-for="pizza in pizzas" :key="pizza.id">
+        {{ pizza.name }}
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
 
 export default {
   name: 'App',
+  data() {
+    return {
+      apiUrl: 'http://localhost:8080/api/1/',
+      pizzas: [],
+    }
+  },
+  methods: {
+    getPizzas() {
+      axios.get(this.apiUrl + "pizza/allPizzas")
+        .then(response => {
+          this.pizzas = response.data
+        })
+        .catch(error => {
+          console.log(error)
+        })
+    }
+  },
+  mounted() {
+    this.getPizzas()
+  }
 }
 </script>
 
